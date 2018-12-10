45c3953060f2b8cd6f0932db3522be019249405c
nickallama <me@nicholasneale.com>
2018-12-10 18:52:07 +0000

    Created base code

---
+++ traffic-montor.py
@@ -0,0 +1,61 @@
+import asyncio
+import grovepi
+import math
+
+analogPins = {
+    'airQuality': 0
+}
+
+digitalPins = {
+    'tempAndHumidity': 4,
+    'waterSensor': 2
+}
+
+grovepi.pinMode(analogPins.get('airQuality'), 'INPUT')
+grovepi.pinMode(digitalPins.get('waterSensor'), 'INPUT')
+
+def getEnvironmentData():
+    temperature, humidity = getTemperatureAndHumidity()
+    data = {
+        'airQuality': getAirQuality(),
+        'temperature': temperature,
+        'humidity': humidity,
+        'water': getWater()
+    }
+
+    for key, value in data.items():
+        if value == None:
+            print('Error with ' + key + ' value. It equals none.')
+            return
+    
+    sendDataOverMQTT(data)
+
+def sendDataOverMQTT(data):
+    print('send data here')
+
+def getAirQuality():
+    try:
+        return grovepi.analogRead(analogPins.get('airQuality'))
+    except IOError:
+        print('IO Error')
+        return None
+
+def getTemperatureAndHumidity():
+    try:
+        [temp, humidity] = grovepi.dht(digitalPins.get('tempAndHumidity'), 0)
+
+        if math.isnan(temp) == False and math.isnan(humidity) == False:
+            return temp, humidity
+        else 
+            return None, None
+    except IOError: 
+        print('IO Error')
+        return None, None
+
+def getWater():
+    try: 
+        return grovepi.digitalRead(grovepi.digitalRead(digitalPins.get('waterSensor')))
+    except IOError:
+        print('IO Error')
+        return None
+
